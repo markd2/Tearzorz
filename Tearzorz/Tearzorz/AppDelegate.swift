05296b4a274af72ccea5cc8f21dc30aa6dc40d3d
MarkD <markd@borkware.com>
2026-01-11 14:30:43 -0500

    new mac app from template

---
+++ Tearzorz/Tearzorz/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Tearzorz
+//
+//  Created by MarkD on 1/11/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Tearzorz/TearzorzTests/TearzorzTests.swift
@@ -0,0 +1,17 @@
+//
+//  TearzorzTests.swift
+//  TearzorzTests
+//
+//  Created by MarkD on 1/11/26.
+//
+
+import Testing
+@testable import Tearzorz
+
+struct TearzorzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
