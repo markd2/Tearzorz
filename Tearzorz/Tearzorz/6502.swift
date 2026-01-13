1881a31b823144a4caf9d9bb169c5e48549d31e7
MarkD <markd@borkware.com>
2026-01-13 14:11:41 -0500

    Start with instructions.  CLC and LDA #xx

--- Tearzorz/Tearzorz/6502.swift
+++ Tearzorz/Tearzorz/6502.swift
@@ -2,6 +2,8 @@
 
 import Foundation
 
+typealias InstructionHandler = (Instruction) -> Void
+
 class MOS6502 {
     var instructions: [Instruction] = []
 
@@ -16,9 +18,85 @@ class MOS6502 {
     // Eventually this will break out into "outside the 6502". maybe.
     let RAM: [UInt8] = Array<UInt8>(repeating: 0, count: 64 * 1024)
 
+    var handlers: [Opcode: InstructionHandler] = [:]
+
+    init() {
+        setupHandlers()
+    }
+
+    func setupHandlers() {
+        handlers[CLC] = handleCLC
+        handlers[LDA] = handleLDA
+    }
+    
+    // for extension
+    let bit0: UInt8 = 1 << 0
+    let bit1: UInt8 = 1 << 1
+    let bit2: UInt8 = 1 << 2
+    let bit3: UInt8 = 1 << 3
+    let bit4: UInt8 = 1 << 4
+    let bit5: UInt8 = 1 << 5
+    let bit6: UInt8 = 1 << 6
+    let bit7: UInt8 = 1 << 7
+}
+
+/// instruction execution
+extension MOS6502 {
+    func addressedByte(_ instruction: Instruction) -> UInt8 {
+        switch instruction.addressingMode {
+        case Accumulator:
+            return accumulator.value
+        case Immediate:
+            return instruction.modeByteValue()
+/*
+        case Absolute:
+        case Absolute_XIndexed:
+        case Absolute_YIndexed:
+        case Implied:
+        case Indirect:
+        case Indexed_Indirect_X:
+        case Indirect_Indexed_Y:
+        case Relative:
+        case ZeroPage:
+        case ZeroPage_XIndexed:
+        case ZeroPage_YIndexed:
+*/
+        default:
+            print("oops")
+            return 0xff
+        }
+    }
+
+    func updateFlags(for byte: UInt8) {
+        if byte == 0 {
+            psw.setFlag(.Z)
+        }
+        if byte & bit7 != 0 {
+            psw.setFlag(.N)
+        }
+    }
+
+    func handleCLC(_ instruction: Instruction) {
+        psw.clearFlag(.C)
+    }
+
+    func handleLDA(_ instruction: Instruction) {
+        let byte = addressedByte(instruction)
+        accumulator.value = byte
+        updateFlags(for: byte)
+    }
+
     // do all the work to do the instruction except for incrementing
     // the program counter
     func execute(_ instruction: Instruction) {
-        print("doing \(instruction)")
+        guard let handler = handlers[instruction.opcode] else {
+            print("no handler for \(instruction)")
+            return
+        }
+
+        handler(instruction)
     }
+
 }
+
+extension Opcode: Hashable { }
