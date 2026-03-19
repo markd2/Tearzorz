223655931a6cbaf28fb1aa84478198bd871d6172
MarkD <markd@borkware.com>
2026-03-19 17:22:08 -0400

    Trigger parser on tearing window load with pre-canned contents

--- Tearzorz/Tearzorz/TearingWindowController.swift
+++ Tearzorz/Tearzorz/TearingWindowController.swift
@@ -6,11 +6,21 @@ class TearingWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-        bootstrapWithKim1File()
+        do {
+            try bootstrapWithKim1File()
+        } catch {
+            Swift.print("oh noes: \(error)")
+        }
     }
 
-    func bootstrapWithKim1File() {
-        
+    func bootstrapWithKim1File() throws {
+        let kim1tapeURL = Bundle.main.url(forResource: "farmer-brown",
+                                          withExtension: "txt")!
+        let kim1tapeBytes = try String(contentsOf: kim1tapeURL,
+                                   encoding: .utf8)
+        let parser = MOSFileParser(withContents: kim1tapeBytes)
+        let (baseAddress, bytes) = try parser.parse()
+        Swift.print(baseAddress, bytes)
     }
     
 }
